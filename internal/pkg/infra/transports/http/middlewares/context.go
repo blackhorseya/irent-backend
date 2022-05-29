cacdaae82ef1bf3d11b98c058fd826be793424d1
Sean Zheng <blackhorseya@gmail.com>
2022-05-29 08:27:36 +0800

    refactor: remove trace

--- internal/pkg/infra/transports/http/middlewares/context.go
+++ internal/pkg/infra/transports/http/middlewares/context.go
@@ -2,14 +2,13 @@ package middlewares
 
 import (
 	"github.com/blackhorseya/gocommon/pkg/contextx"
-	"github.com/blackhorseya/irent/internal/pkg/base/trace"
 	"github.com/gin-gonic/gin"
 )
 
 // ContextMiddleware serve caller to added Contextx into gin
 func ContextMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx := contextx.WithValue(contextx.Background(), "traceID", trace.NewTraceID())
+		ctx := contextx.Background()
 		c.Set("ctx", ctx)
 
 		c.Next()
