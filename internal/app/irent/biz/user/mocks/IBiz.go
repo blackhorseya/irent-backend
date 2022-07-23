d01d420467b2fd965f0db9108302faac11f87a69
Sean Zheng <blackhorseya@gmail.com>
2022-07-24 06:10:58 +0800

    refactor: remove useless methods

--- internal/app/irent/biz/user/mocks/IBiz.go
+++ internal/app/irent/biz/user/mocks/IBiz.go
@@ -14,52 +14,6 @@ type IBiz struct {
 	mock.Mock
 }
 
-// GetByAccessToken provides a mock function with given fields: ctx, token
-func (_m *IBiz) GetByAccessToken(ctx contextx.Contextx, token string) (*user.Profile, error) {
-	ret := _m.Called(ctx, token)
-
-	var r0 *user.Profile
-	if rf, ok := ret.Get(0).(func(contextx.Contextx, string) *user.Profile); ok {
-		r0 = rf(ctx, token)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*user.Profile)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(contextx.Contextx, string) error); ok {
-		r1 = rf(ctx, token)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
-// GetByID provides a mock function with given fields: ctx, id
-func (_m *IBiz) GetByID(ctx contextx.Contextx, id string) (*user.Profile, error) {
-	ret := _m.Called(ctx, id)
-
-	var r0 *user.Profile
-	if rf, ok := ret.Get(0).(func(contextx.Contextx, string) *user.Profile); ok {
-		r0 = rf(ctx, id)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*user.Profile)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(contextx.Contextx, string) error); ok {
-		r1 = rf(ctx, id)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // Login provides a mock function with given fields: ctx, id, password
 func (_m *IBiz) Login(ctx contextx.Contextx, id string, password string) (*user.Profile, error) {
 	ret := _m.Called(ctx, id, password)
@@ -76,43 +30,6 @@ func (_m *IBiz) Login(ctx contextx.Contextx, id string, password string) (*user.
 	var r1 error
 	if rf, ok := ret.Get(1).(func(contextx.Contextx, string, string) error); ok {
 		r1 = rf(ctx, id, password)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
-// Logout provides a mock function with given fields: ctx, _a1
-func (_m *IBiz) Logout(ctx contextx.Contextx, _a1 *user.Profile) error {
-	ret := _m.Called(ctx, _a1)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(contextx.Contextx, *user.Profile) error); ok {
-		r0 = rf(ctx, _a1)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
-// RefreshToken provides a mock function with given fields: ctx, _a1
-func (_m *IBiz) RefreshToken(ctx contextx.Contextx, _a1 *user.Profile) (*user.Profile, error) {
-	ret := _m.Called(ctx, _a1)
-
-	var r0 *user.Profile
-	if rf, ok := ret.Get(0).(func(contextx.Contextx, *user.Profile) *user.Profile); ok {
-		r0 = rf(ctx, _a1)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*user.Profile)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(contextx.Contextx, *user.Profile) error); ok {
-		r1 = rf(ctx, _a1)
 	} else {
 		r1 = ret.Error(1)
 	}
--- internal/app/irent/biz/user/user.go
+++ internal/app/irent/biz/user/user.go
@@ -10,20 +10,8 @@ import (
 // IBiz declare user biz service function
 //go:generate mockery --name=IBiz
 type IBiz interface {
-	// GetByID serve caller to given user's id to get user profile
-	GetByID(ctx contextx.Contextx, id string) (info *user.Profile, err error)
-
-	// GetByAccessToken serve caller to given user's access token to get user profile
-	GetByAccessToken(ctx contextx.Contextx, token string) (info *user.Profile, err error)
-
 	// Login serve caller to given id and password then login the system
 	Login(ctx contextx.Contextx, id, password string) (info *user.Profile, err error)
-
-	// RefreshToken serve caller to refresh user's access token
-	RefreshToken(ctx contextx.Contextx, user *user.Profile) (info *user.Profile, err error)
-
-	// Logout serve caller to log out the system
-	Logout(ctx contextx.Contextx, user *user.Profile) error
 }
 
 // ProviderSet is a provider set for wire
